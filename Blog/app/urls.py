a8a0eefcca51fe512fc479284b50553c58fa425e
itssahilwhat <bhatiasahil1515@gmail.com>
2024-11-28 21:05:06 +0530

    plan

--- Blog/app/urls.py
+++ Blog/app/urls.py
@@ -1,1 +1,9 @@
 from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name='blog-home'),
+    path('posts', views.posts, name='blog-posts'),
+    path('posts/<slug:slug>', views.post, name='blog-post'),
+]
--- Blog/app/views.py
+++ Blog/app/views.py
@@ -1,1 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('<h1>Blog Home</h1>')
+
+
+def posts(request):
+    return HttpResponse('<h1>Blog Posts</h1>')
+
+
+def post(request, post_id):
+    return HttpResponse(f'<h1>Blog Post {post_id}</h1>')
